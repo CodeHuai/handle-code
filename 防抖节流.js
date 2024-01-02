e6c98f03fd1c896dcc0f99fa41f06e2c025e32f1
andbreak <1622170503@qq.com>
2024-01-02 10:53:56 +0800

    init

--- 防抖节流.js
+++ 防抖节流.js
@@ -1,9 +1,9 @@
 function debounce(fn, delay) {
-  let timer = null;
-  return function (...arg) {
-    if (timer) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(, delay);
-  };
+    let timer = null;
+    return function (...arg) {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(fn, delay);
+    };
 }
